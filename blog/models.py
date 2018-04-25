f28b69b8ef9512e2cabecbb73f28b02068da8c65
dismalstarshine <5bzxhn@gmail.com>
2018-04-26 00:36:06 +0800

    修复detail错误

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-#from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 
@@ -24,9 +23,9 @@ class Post(models.Model):
     created_time = models.DateTimeField()
     modified_time = models.DateTimeField()
     excerpt = models.CharField(max_length=200,blank=True)
-    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag, blank=True)
-    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
